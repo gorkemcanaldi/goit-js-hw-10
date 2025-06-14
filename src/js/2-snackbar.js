d3ec0bd6237d154424964ce69f900b17513f6f01
görkem can aldı <gorkem.aldi2003@gmail.com>
2025-06-14 13:13:59 +0300

    snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // Formdan değerleri al
+  const delay = Number(form.elements.delay.value);
+  const state = form.elements.state.value;
+
+  // Promise üret
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: '✅ Fulfilled',
+        message: `Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: '❌ Rejected',
+        message: `Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
+
+// Promise üreteci fonksiyonu
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
